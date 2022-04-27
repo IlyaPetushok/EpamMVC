fe1506e340cdc8893bb12b62b560a11a8bfa08ee
Petushok Ilya <a3310010752@mail.ru>
2022-04-27 16:52:51 +0300

    add check use login and email

--- src/main/java/com/example/epammvc/dao/impl/UserDaoImpl.java
+++ src/main/java/com/example/epammvc/dao/impl/UserDaoImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 public class UserDaoImpl extends BaseDao<User> implements UserDao {
     private static final String SELECT_LOGIN_AND_PASSWORD = "Select * FROM info_user WHERE login = ? and password=?";
     private static final String INSERT_USER = "INSERT INTO info_user(name,login,password,sex,email,data) VALUES (?,?,?,?,?,?);";
+    public static final String CHECK_LOGIN_OR_EMAIL = "Select * FROM info_user WHERE login = ? or email=?";
     private static UserDaoImpl instance = new UserDaoImpl();
 
     private UserDaoImpl() {
@@ -26,21 +27,30 @@ public class UserDaoImpl extends BaseDao<User> implements UserDao {
         try {
             ConnectionBuilder connectionBuilder = ConnectionBuilder.getInstance();
             Connection connection = connectionBuilder.getFreeConnection();
-            PreparedStatement statement = connection.prepareStatement(INSERT_USER);
-            String name = user.getName();
-            String login = user.getLogin();
-            String password = user.getPassword();
-            String sex = user.getSex();
-            String email = user.getEmail();
-            String data = user.getData();
-            statement.setString(1, name);
-            statement.setString(2, login);
-            statement.setString(3, password);
-            statement.setString(4, sex);
-            statement.setString(5, email);
-            statement.setString(6, data);
-            statement.executeUpdate();
-            connectionBuilder.releaseConnection(connection);
+
+            PreparedStatement statement1 = connection.prepareStatement(CHECK_LOGIN_OR_EMAIL);
+            statement1.setString(1, user.getLogin());
+            statement1.setString(2, user.getEmail());
+            ResultSet resultSet= statement1.executeQuery();
+            if (resultSet.next()) {
+                return false;
+            } else {
+                PreparedStatement statement = connection.prepareStatement(INSERT_USER);
+                String name = user.getName();
+                String login = user.getLogin();
+                String password = user.getPassword();
+                String sex = user.getSex();
+                String email = user.getEmail();
+                String data = user.getData();
+                statement.setString(1, name);
+                statement.setString(2, login);
+                statement.setString(3, password);
+                statement.setString(4, sex);
+                statement.setString(5, email);
+                statement.setString(6, data);
+                statement.executeUpdate();
+                connectionBuilder.releaseConnection(connection);
+            }
         } catch (SQLException exception) {
             throw new DaoException("Error in try insert user", exception);
         }
@@ -70,7 +80,7 @@ public class UserDaoImpl extends BaseDao<User> implements UserDao {
             }
             connectionBuilder.releaseConnection(connection);
         } catch (SQLException exception) {
-            throw new DaoException("Error in try input",exception);
+            throw new DaoException("Error in try input", exception);
         }
         return new User(id, name, sex, data, email);
     }
