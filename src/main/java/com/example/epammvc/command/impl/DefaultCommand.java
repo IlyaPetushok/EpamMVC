9a3ff032d8f129bcbd89ea9b2da0621c1f6947b3
Petushok Ilya <a3310010752@mail.ru>
2022-05-09 19:38:21 +0300

    add Router

--- src/main/java/com/example/epammvc/command/impl/DefaultCommand.java
+++ src/main/java/com/example/epammvc/command/impl/DefaultCommand.java
@@ -3,12 +3,13 @@ package com.example.epammvc.command.impl;
 import com.example.epammvc.command.Command;
 import com.example.epammvc.controller.Router;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class DefaultCommand implements Command {
     private static final String HOMEPAGE = "index.jsp";
 
     @Override
-    public Router execute(HttpServletRequest request) {
+    public Router execute(HttpServletRequest request, HttpServletResponse response) {
         Router router=new Router();
         router.setPage(HOMEPAGE);
         return router;
--- src/main/java/com/example/epammvc/command/impl/LogoutCommand.java
+++ src/main/java/com/example/epammvc/command/impl/LogoutCommand.java
@@ -4,12 +4,13 @@ import com.example.epammvc.command.Command;
 import com.example.epammvc.controller.Router;
 import com.example.epammvc.exception.CommandException;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class LogoutCommand implements Command {
     private static final String HOMEPAGE = "index.jsp";
 
     @Override
-    public Router execute(HttpServletRequest request) throws CommandException {
+    public Router execute(HttpServletRequest request, HttpServletResponse response) throws CommandException {
         Router router=new Router();
         router.setPage(HOMEPAGE);
         request.getSession().invalidate();
