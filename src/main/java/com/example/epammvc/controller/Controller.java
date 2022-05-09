87515d49061e141692e057e8dcf3dfa973d34066
Petushok Ilya <a3310010752@mail.ru>
2022-05-09 19:34:20 +0300

    add method doPost

--- src/main/java/com/example/epammvc/controller/Controller.java
+++ src/main/java/com/example/epammvc/controller/Controller.java
@@ -12,9 +12,10 @@ import jakarta.servlet.annotation.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-
+@MultipartConfig
 @WebServlet(name = "helloServlet", value = "/home")
 public class Controller extends HttpServlet {
+    public static final String  COMMAND="command";
     public static final Logger logger = LogManager.getLogger(Controller.class.getName());
 
     public void init() {
@@ -22,12 +23,47 @@ public class Controller extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        String commandStr = request.getParameter("command");
-        Command command = CommandType.getCommand(commandStr);
+        String commandStr = request.getParameter(COMMAND);//const
+        Command command = CommandType.getCommand(commandStr);//valid
         String page = null;
         try {
             Router router = new Router();
-            router = command.execute(request);
+            router = command.execute(request,response);
+//            String type=router.getType();
+            switch (router.getType()) {
+                case REDIRECT:
+                    response.sendRedirect(request.getContextPath() + router.getPage());
+                    break;
+
+                case FORWARD:
+                    request.getRequestDispatcher(router.getPage()).forward(request, response);
+                    break;
+
+                default:
+                    logger.error("Invalid router dispatch type : {}", router.getType());
+//                    response.sendError(500);
+
+            }
+//            request.getRequestDispatcher(page).forward(request,response);
+//            response.sendRedirect(request.getContextPath()+page);
+        } catch (CommandException exception) {
+            request.setAttribute("error", exception);
+            request.getRequestDispatcher("pages/error/error_500.jsp").forward(request, response);
+            throw new ServletException(exception);
+        }
+        System.out.println("page" + page);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("multipart/form-data");
+        String commandStr = request.getParameter(COMMAND);//const
+        Command command = CommandType.getCommand(commandStr);//valid
+        String page = null;
+        try {
+            Router router = new Router();
+            router = command.execute(request,response);
 //            String type=router.getType();
             switch (router.getType()) {
                 case REDIRECT:
